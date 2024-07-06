5e58b4e029fdf68cdbc7f13cf0d0202c369394c2
vanessagiannoni <vanessa.o.giannoni@gmail.com>
2024-07-05 21:35:13 -0300

    feat: add home page texts

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,23 +1,41 @@
-import { Title, Text, Anchor } from '@mantine/core';
+'use client';
+
+import { Title, Text, Container } from '@mantine/core';
 import classes from './Welcome.module.css';
+import useBreakpoint from '@/src/hooks/useBreakpoint';
+import ResumeButton from '../ResumeButton/ResumeButton';
 
 export function Welcome() {
+  const isSmallScreen = useBreakpoint('md');
+
   return (
-    <>
-      <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+    <div className={classes.sectionContainer}>
+      <div className={classes.helloTitle}>
+        <Title ta="left" mt={100} size={48} className={classes.helloTitle}>
+            Hello there!{' '}
+            <Text className={classes.thinFont} size="32px">
+              <em>My name is...</em>
+            </Text>
+        </Title>
+      </div>
+      <Container>
+        <div className={classes.nameTitle}>
+          <Title className={classes.title} mt={100}>
+            <strong>Vanessa Giannoni, or</strong>
+            <Text inherit fs="italic" ta="left" className={classes.thinFont}><strong>just</strong>&nbsp;<em>Vans</em></Text>
+          </Title>
+        </div>
+        <Text size="28px" mt="xl" fw={400}>
+          And I&apos;m a builder! Well...something like that
         </Text>
-      </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit page.tsx file.
-      </Text>
-    </>
+
+      </Container>
+
+      {!isSmallScreen && (
+        <div className={classes.resumeButton}>
+          <ResumeButton />
+        </div>
+      )}
+    </div>
   );
 }
