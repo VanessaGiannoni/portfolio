b07b95c62de12972717f3f9e8439723d9d40dc56
vanessagiannoni <vanessa.o.giannoni@gmail.com>
2024-07-05 21:33:17 -0300

    style: refactor resume button

--- src/components/ResumeButton/ResumeButton.tsx
+++ src/components/ResumeButton/ResumeButton.tsx
@@ -1,17 +1,14 @@
 'use client';
 
-import { UnstyledButton, useMantineColorScheme } from '@mantine/core';
+import { UnstyledButton } from '@mantine/core';
 import classes from './resume-button.module.css';
 
 export default function ResumeButton() {
-  const { colorScheme } = useMantineColorScheme();
-  const buttonClass = colorScheme === 'light' ? classes.lightButton : classes.darkButton;
-
   return (
     <UnstyledButton
-      className={`${buttonClass} ${classes.buttonBase}`}
+      className={classes.buttonBase}
     >
-        See Resume
+      See Resume
     </UnstyledButton>
   );
 }
