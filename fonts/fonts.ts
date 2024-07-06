be5f97d26afa6c3e7f10b5be92e52cacb62a9e28
vanessagiannoni <vanessa.o.giannoni@gmail.com>
2024-07-05 21:31:03 -0300

    style: add font family

---
+++ fonts/fonts.ts
@@ -0,0 +1,11 @@
+import { Noto_Sans, Inter } from 'next/font/google';
+
+export const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['100', '400', '700'],
+});
+
+export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '400', '700'],
+});
