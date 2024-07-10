95e0394e5a853d8599bd7edf164616edc948498f
vanessagiannoni <vanessa.o.giannoni@gmail.com>
2024-07-09 22:26:50 -0300

    feat: create subcomponents for experience section

---
+++ src/components/Experience/subcomponents/TimeLineItem.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { Timeline, Title, Avatar, Text } from '@mantine/core';
+
+interface TimeLineItemProps {
+  title: string;
+  jobTitle: string;
+  period: string;
+  src: string;
+  altText: string;
+  description: string;
+  stack: string;
+}
+
+export default function TimeLineItem({
+  title,
+  jobTitle,
+  period,
+  src,
+  altText,
+  description,
+  stack,
+}: TimeLineItemProps) {
+  return (
+    <Timeline.Item
+      title={
+        <>
+          <Title order={4} size={20}>
+            {title}
+          </Title>
+          <Text size="xs" mt={4} fw={700}>
+            {jobTitle} - {period}
+          </Text>
+        </>
+      }
+      lineVariant="dotted"
+      bullet={<Avatar src={src} variant="filled" size={48} alt={altText} />}
+    >
+      <Text size="sm" pt={10}>
+        {description}
+      </Text>
+      <Text size="xs" mt={4} fw={700}>
+        Skills: {stack}
+      </Text>
+    </Timeline.Item>
+  );
+}
