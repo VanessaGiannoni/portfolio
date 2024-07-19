116abdec009b5aada24a6a0d10bfa4c57712dc07
vanessagiannoni <vanessa.o.giannoni@gmail.com>
2024-07-18 23:47:27 -0300

    feat: add sections translation

--- src/components/ResumeButton/ResumeButton.tsx
+++ src/components/ResumeButton/ResumeButton.tsx
@@ -2,17 +2,21 @@
 
 import { UnstyledButton } from '@mantine/core';
 import Link from 'next/link';
+
+import { useTranslations } from 'next-intl';
 import classes from './resume-button.module.css';
 
 export default function ResumeButton() {
+  const t = useTranslations('header');
   return (
     <UnstyledButton
       className={classes.buttonBase}
       component={Link}
       href="/vanessa-resume.pdf"
       target="_blank"
+      title="see_resume"
     >
-      See Resume
+      {t('see_resume')}
     </UnstyledButton>
   );
 }
